790a84ed632d23542bdae470e31039ccdfe19c0a
choi-w-s <ccwwsss@naver.com>
2022-05-07 17:38:44 +0900

    캘린더 추가 도중 인식불가 오류

---
+++ lmsPlus/lmsPlus/ICalendarEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace lmsPlus
+{
+    public interface ICalendarEvent
+    {
+        DateTime? DateFrom { get; set; }
+        DateTime? DateTo { get; set; }
+        string Label { get; set; }
+    }
+}
